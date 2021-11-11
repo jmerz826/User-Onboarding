716dbe4094e7a62c2f5ceaf4020a1b211e1e7288
John Merz <jmerz826@gmail.com>
2021-11-11 16:01:58 -0500

    existence tests are passing

--- user-onboarding/cypress/integration/form_test.js
+++ user-onboarding/cypress/integration/form_test.js
@@ -7,14 +7,27 @@ describe('Onboarding App', () => {
     })
 
     const nameInput = () => cy.get('input[name=username]');
+    const emailInput = () => cy.get('input[name=email]');
+    const passwordInput = () => cy.get('input[name=password]');
+    const termsInput = () => cy.get('input[name=terms]');
+
 
     it('sanity check', () => {
         expect(1 + 2).to.equal(3);
         expect(2 + 2).not.to.equal(5);
     })
 
-    it('elements check', () => {
+    it('elements existence check', () => {
         nameInput().should('exist');
+        emailInput().should('exist');
+        passwordInput().should('exist');
+        termsInput().should('exist');
+    })
+
+    describe('Form input functionality', () => {
+        
+
+
     })
 
 
